e214e2336f23a6defed443f94febbc9e6f379f39
isantidev <santis.sierra6@gmail.com>
2025-12-30 17:32:31 -0500

    Refactor type imports and paths from '@types' to '@tstypes', because of malfunctioning imports.

--- src/context/Login.tsx
+++ src/context/Login.tsx
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import { persist, createJSONStorage } from "zustand/middleware";
-import type { AuthState } from "@types/auth";
+import type { AuthState } from "@tstypes/auth";
 
 export const useAuth = create<AuthState>()(
     persist(
