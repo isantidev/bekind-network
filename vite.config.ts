776664718b5947f651fcedf383e0500ff57eb103
isantidev <santis.sierra6@gmail.com>
2025-12-30 17:19:40 -0500

    Add react-router-dom dependency and update TypeScript configuration paths

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
             "@routes": path.resolve(__dirname, "./src/routes"),
             "@styles": path.resolve(__dirname, "./src/styles"),
             "@utils": path.resolve(__dirname, "./src/utils"),
+            "@types": path.resolve(__dirname, "./src/types"),
         },
     },
 });
